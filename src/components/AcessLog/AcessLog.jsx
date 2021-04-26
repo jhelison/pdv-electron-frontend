da516519149ec33b5fcb21c04c783f4b0eb3c8cc
Jhelison <jhelisong@gmail.com>
2021-04-26 10:59:52 -0300

    Added filter on changeLog table

--- src/components/AcessLog/AcessLog.jsx
+++ src/components/AcessLog/AcessLog.jsx
@@ -1,7 +1,19 @@
-import React from "react"
+import React, { useState, useEffect, forwardRef } from "react"
 import Main from "../template/Main"
 
 import { BsClockHistory } from "react-icons/bs"
+
+import Select from "react-select"
+import DatePicker, { registerLocale } from "react-datepicker"
+import ptBR from "date-fns/locale/pt-BR"
+
+const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      color: state.isSelected ? 'black' : 'black',
+    })
+}
+
 
 const acessData = [
     ["10/12/2021", "Jhelison Uchoa", "Orçamento", "Adicionado"],
@@ -13,7 +25,18 @@ const acessData = [
     ["10/12/2021", "Mark", "Clientes", "Deletado"],
 ]
 
+const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+]
+
 export default (props) => {
+    const [startDate, setStartDate] = useState(new Date())
+    const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+        <input className="form-control" type="text" onClick={onClick} ref={ref} placeholder="Default input" value={startDate.getDay() + "/" + startDate.getMonth() + "/" + startDate.getFullYear()}/>
+    ))
+
     const buildTable = () => {
         return (
             <table className="table table-sm">
@@ -28,6 +51,26 @@ export default (props) => {
                             </div>
                         </th>
                     </tr>
+                    <tr>
+                        <th scope="col" className="w-15">
+                            <DatePicker
+                                selected={startDate}
+                                onChange={(date) => setStartDate(date)}
+                                customInput={<ExampleCustomInput />}
+                                locale={ptBR}
+                                todayButton="Hoje"
+                            />
+                        </th>
+                        <th scope="col">
+                            <Select options={options} styles={customStyles} isMulti/>
+                        </th>
+                        <th scope="col">
+                            <Select options={options} styles={customStyles} isMulti/>
+                        </th>
+                        <th scope="col">
+                            <Select options={options} styles={customStyles} isMulti/>
+                        </th>
+                    </tr>
                 </thead>
                 {buildTableRows()}
             </table>
@@ -38,16 +81,26 @@ export default (props) => {
         const getTypeOfChange = (text) => {
             if (text === "Adicionado") {
                 return (
-                    <div className="alert-custom alert-success-custom">{text}</div>
+                    <div className="alert-custom alert-success-custom">
+                        {text}
+                    </div>
                 )
             }
             if (text === "Atualizado") {
-                return <div className="alert-custom alert-alert-custom">{text}</div>
+                return (
+                    <div className="alert-custom alert-alert-custom">
+                        {text}
+                    </div>
+                )
             }
             if (text === "Visualizado") {
-                return <div className="alert-custom alert-info-custom">{text}</div>
+                return (
+                    <div className="alert-custom alert-info-custom">{text}</div>
+                )
             }
-            return <div className="alert-custom alert-danger-custom">{text}</div>
+            return (
+                <div className="alert-custom alert-danger-custom">{text}</div>
+            )
         }
 
         return (
@@ -76,7 +129,6 @@ export default (props) => {
             subtitle="Veja tudo que foi feito pelo aplicativo"
             icon={<BsClockHistory />}
         >
-            <div className="small-card mb-3 d-flex justify-content-end"></div>
             <div className="card user-card">{buildTable()}</div>
         </Main>
     )
