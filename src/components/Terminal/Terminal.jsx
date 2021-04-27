dfdfa6b73fc1dd72ed6e03201d73177ac8860c7c
Jhelison <jhelisong@gmail.com>
2021-04-27 10:58:59 -0300

    Added reset button

--- src/components/Terminal/Terminal.jsx
+++ src/components/Terminal/Terminal.jsx
@@ -3,6 +3,7 @@ import Main from "../template/Main"
 
 import { FiCheck } from "react-icons/fi"
 import { FiX } from "react-icons/fi"
+import { FiRotateCw } from "react-icons/fi"
 
 import { BsTerminal } from "react-icons/bs"
 
@@ -26,11 +27,11 @@ export default (props) => {
     const getServerStatus = (statusBlueprint) => {
         if (statusBlueprint) {
             return (
-                <button type="button" className="btn btn-success btn-sm w-25 align-self-end" disabled><FiCheck /> 356 ms</button>
+                <button type="button" className="btn btn-success w-25 btn-sm no-click"><FiCheck /> 356 ms</button>
             )
         }
         return (
-            <button type="button" className="btn btn-danger btn-sm w-25 align-self-end"><FiX /> Pressione para reinicializar</button>
+            <button type="button" className="btn btn-danger w-25 btn-sm no-click"><FiX /></button>
         )
     }
 
@@ -42,7 +43,11 @@ export default (props) => {
         >
             <div className="small-card mb-3 d-flex justify-content-between align-items-center flex-row">
                 <span>Status atual do servidor</span>
-                {getServerStatus(true)}
+                <div className="w-50 d-flex justify-content-end">
+                    {getServerStatus(false)}
+                    <button type="button" className="btn btn-secondary w-25 btn-sm align-self-end ml-1"><FiRotateCw /></button>
+                </div>
+                
             </div>
             <div className="card terminal-card">
                 <pre>{pythonShellText}</pre>
